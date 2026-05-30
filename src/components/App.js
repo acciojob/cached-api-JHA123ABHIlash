00cdb623c38894f9d4a1953b35ea72361d1a9922
Abhilash Jha <abhilashjha121@gmail.com>
2026-05-30 20:00:49 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,54 @@
-
-import React from "react";
+import "regenerator-runtime/runtime";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const url='https://jsonplaceholder.typicode.com/posts';
+
+  const [data,setData]=useState([]);
+  const [loading,setLoading]=useState(false);
+
+async function fetchdata() {
+    setLoading(true);
+    try {
+      const res=await fetch(url);
+      const value=await res.json();
+      setData(value);
+     
+    } catch (error) {
+      setData([]);
+      console.log(error);
+    }
+    setLoading(false);
+  }
+  
+
+  const memoizedData = useMemo(() => {
+  return data;
+}, [data]);
+
+  useEffect(()=>{
+    fetchdata()
+     
+  },[]);
+
+console.log(data);
   return (
     <div>
-        {/* Do not remove the main div */}
+     {
+      loading ? (<div>Loading...</div>):(
+         <ul>
+        {
+       memoizedData.map((el)=>(
+           <li>
+           <b> {el.title}</b>
+            <p>{el.body}</p>
+          </li>
+        ))
+      }
+      </ul>
+      )
+     }
     </div>
   )
 }
